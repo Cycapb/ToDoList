3558c57cdd9577ea82450c62db5df72a11bb3d02
Cycapb <aka.apostol@gmail.com>
2016-05-22 11:26:28 +0500

    Created abstract Irepository and concrete EntityReposytory. Created model.

---
+++ ToDoDAL/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ToDoDAL.Abstract
+{
+    public interface IRepository<T>:IDisposable where T:class
+    {
+        IEnumerable<T> GetList();
+        Task<IEnumerable<T>> GetListAsync();
+        void Create(T item);
+        Task CreateAsync(T item);
+        void Delete(int id);
+        Task DeleteAsync(int id);
+        void Update(T item);
+        Task UpdateAsync(T item);
+        void Save();
+        Task SaveAsync();
+    }
+}
---
+++ ToDoDAL/Concrete/EntityRepository.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using ToDoDAL.Abstract;
+using ToDoDAL.Model;
+
+namespace ToDoDAL.Concrete
+{
+    public class EntityRepository<T>:IRepository<T> where T:class
+    {
+        private readonly todoEntities _context;
+        private readonly DbSet<T> _dbSet;
+
+        private bool _disposed = false;
+        
+        public EntityRepository()
+        {
+            _context = new todoEntities();
+            _dbSet = _context.Set<T>();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this._disposed = true;
+        }
+
+        public IEnumerable<T> GetList()
+        {
+            return _dbSet;
+        }
+
+        public Task<IEnumerable<T>> GetListAsync()
+        {
+            return Task.Run(() =>
+            {
+                IEnumerable<T> list = _dbSet;
+                return list;
+            });
+        }
+
+        public void Create(T item)
+        {
+            _dbSet.Add(item);
+        }
+
+        public Task CreateAsync(T item)
+        {
+            return Task.Run(()=>_dbSet.Add(item));
+        }
+
+        public void Delete(int id)
+        {
+            var itemToDel = _dbSet.Find(id);
+            if (itemToDel != null)
+            {
+                _dbSet.Remove(itemToDel);
+            }
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            return Task.Run(async () =>
+            {
+                var itemToDel = await _dbSet.FindAsync(id);
+                if (itemToDel != null)
+                {
+                    _dbSet.Remove(itemToDel);
+                }
+            });
+        }
+
+        public void Update(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        public Task UpdateAsync(T item)
+        {
+            return Task.Run(() =>
+            {
+                _context.Entry(item).State = EntityState.Modified;
+            });
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
