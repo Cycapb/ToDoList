49e453d5d946a697bb11a0290453f6625a762f74
Cycapb <aka.apostol@gmail.com>
2016-05-24 14:05:46 +0500

    Added nuget pacckages to make SAP

---
+++ ToDoWebAPI/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace ToDoWebAPI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css"));
+
+            bundles.Add(new ScriptBundle("/Script/knockout").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/knockout-{version}.js"));
+        }
+    }
+}
