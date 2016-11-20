a1594986bbfc7e07c710cf4ecd0e65f20f6988be
Cycapb <aka.apostol@gmail.com>
2016-11-20 17:10:15 +0500

    Deleted edmx model and created two new models for CodeFirst - Group and ToDoList. Created new TodoContext

---
+++ ToDoDAL/Model/TodoContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+
+namespace ToDoDAL.Model
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext():base("name=todoEntities")
+        {
+            
+        }
+
+        public DbSet<ToDoList> ToDoList { get; set; }
+        public DbSet<Group> Group { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Group>()
+                .HasKey(s => s.GroupId);
+            modelBuilder.Entity<Group>()
+                .Property(s => s.Name)
+                .HasMaxLength(50);
+            modelBuilder.Entity<Group>()
+                .Property(s => s.UserId)
+                .IsRequired();
+        }
+    }
+}
