16f9fa2001f89df0ee8fd8d586e6557968c81cff
Susarev Andrey <aka.apostol@gmail.com>
2016-11-21 14:49:21 +0500

    Deleted wrong properties GroupName from GetValueAsync method of ToDoListProvider

--- ToDoWebAPI/Concrete/ToDoListProvider.cs
+++ ToDoWebAPI/Concrete/ToDoListProvider.cs
@@ -24,7 +24,6 @@ namespace ToDoWebAPI.Concrete
                     Comment = x.Comment,
                     Name = x.Name,
                     GroupId = x.GroupId,
-                    GroupName = x.Group.Name,
                     StatusId = x.StatusId,
                     UserId = x.UserId,
                     NoteId = x.NoteId
@@ -45,7 +44,6 @@ namespace ToDoWebAPI.Concrete
                     Comment = item.Comment,
                     Name = item.Name,
                     GroupId = item.GroupId,
-                    GroupName = item.Group.Name,
                     StatusId = item.StatusId,
                     UserId = item.UserId,
                     NoteId = item.NoteId
