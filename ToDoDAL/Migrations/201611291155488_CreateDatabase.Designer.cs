a650e3e97254577687f53500f9e9137ec3a73c21
Susarev Andrey <aka.apostol@gmail.com>
2016-11-29 17:02:41 +0500

    Added initial migration for ToDoList

--- ToDoDAL/Migrations/201611291155488_CreateDatabase.Designer.cs
+++ ToDoDAL/Migrations/201611291155488_CreateDatabase.Designer.cs
@@ -7,13 +7,13 @@ namespace ToDoDAL.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class CreateDatabase : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CreateDatabase));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611211140419_InitialCreate"; }
+            get { return "201611291155488_CreateDatabase"; }
         }
         
         string IMigrationMetadata.Source
