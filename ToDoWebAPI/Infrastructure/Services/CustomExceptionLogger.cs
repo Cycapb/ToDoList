c8213ca171120a408014a2c093f5920f20b1b389
Susarev Andrey <aka.apostol@gmail.com>
2016-12-03 16:40:43 +0500

    Added CustomExceptionLogger:ExceptionLogger. Moved WebUI project from framework version 4.5.2 to 4.6

---
+++ ToDoWebAPI/Infrastructure/Services/CustomExceptionLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using NLog;
+
+namespace ToDoWebAPI.Infrastructure.Services
+{
+    public class CustomExceptionLogger:ExceptionLogger
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        public override Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            var message = new StringBuilder();
+            message.AppendLine($"Request URI: {context.Request.RequestUri.AbsolutePath}");
+            message.AppendLine("Headers: ");
+            foreach (var header in context.Request.Headers)
+            {
+                message.AppendLine($"{header.Key}: {string.Join(",",header.Value)}");
+            }
+            message.AppendLine("Cookies:");
+            foreach (var cookie in context.Request.Headers.GetCookies())
+            {
+                foreach (var cookieItem in cookie.Cookies)
+                {
+                    var values = cookieItem.Values?.Cast<string>().Select(x => cookieItem.Values[x]);
+                    var str = string.Join(",",values?? Enumerable.Empty<string>());
+                    message.AppendLine($"{cookieItem.Name} : {str}");
+                }
+            }
+            _logger.Log(LogLevel.Error,context.Exception, $"Data: {message}");
+            return Task.CompletedTask;
+        }
+    }
+}
