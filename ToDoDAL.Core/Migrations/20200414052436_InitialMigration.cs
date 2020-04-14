62eecf42379097b8ba594b7f4aae9f76358178eb
Cycapb <aka.apostol@gmail.com>
2020-04-14 10:27:12 +0500

    Registered ToDoContextCore in Startup.cs. Added migrations to ToDoDAL.Core project.

---
+++ ToDoDAL.Core/Migrations/20200414052436_InitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoDAL.Core.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoGroup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    UserId = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoGroup", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(maxLength: 1024, nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    IsFinished = table.Column<bool>(nullable: false),
+                    UserId = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoItem_TodoGroup_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "TodoGroup",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItem_GroupId",
+                table: "TodoItem",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+
+            migrationBuilder.DropTable(
+                name: "TodoGroup");
+        }
+    }
+}
--- ToDoWebAPI.Core/Startup.cs
+++ ToDoWebAPI.Core/Startup.cs
@@ -1,16 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDoDAL.Core.Model;
 
 namespace ToDoWebAPI.Core
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContextCore>(options => 
+            {
+                options.UseLazyLoadingProxies();
+                options.UseSqlServer(_configuration["ConnectionStrings:TodoEntities:ConnectionString"]);
+            });
             services.AddMvc().AddMvcOptions(setupAction => setupAction.EnableEndpointRouting = false);
         }
 
